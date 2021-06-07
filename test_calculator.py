e82770bed948322fb15779474b2da5335f9e5e23
allisonrenie <allisonrenie@gmail.com>
2021-06-06 23:00:08 -0700

    tests for mult and div

--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,12 @@ class TestCase(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(calculator.subtract(10, 3), 7)
 
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(7, 1), 7)
+
+    def test_divide(self):
+        self.assertEqual(calculator.divide(28, 4), 7)
+
 
 if __name__ == '__main__':
     unittest.main()
